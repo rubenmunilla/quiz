e652eada2d65c658eedf0c57427e90622345a850
Rubén <rubenmunilla@gmail.com>
2015-08-03 18:47:26 +0200

    Edición de preguntas
    Edición de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,5 +1,30 @@
 var models = require('../models/models.js');
 
+exports.edit = function(req, res) {
+  var quiz = req.quiz; 
+
+  res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+exports.update = function(req, res){
+	req.quiz.pregunta  = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+
+	req.quiz
+	.validate()
+	.then(
+		function(err){
+			if (err) {
+				res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+			} else {
+				req.quiz
+				.save( {fields: ["pregunta", "respuesta"]})
+				.then( function(){ res.redirect('/quizes');});
+      }
+    }
+  );
+};
+
 exports.new = function(req, res) {
   var quiz = models.Quiz.build( 
     {pregunta: "Pregunta", respuesta: "Respuesta"}
@@ -22,7 +47,7 @@ exports.create = function(req, res) {
 };
 
 exports.load = function(req, res,next,quizId) {
-	models.Quiz.findById(quizId).then(
+	models.Quiz.find(quizId).then(
 		function(quiz){
 			if(quiz){
 				req.quiz=quiz;
@@ -33,7 +58,7 @@ exports.load = function(req, res,next,quizId) {
 	).catch(function(error){next(error);});
 };
 
-exports.index=function(req,res){
+exports.index = function(req,res){
 	var busqueda = "";
     if(typeof req.query.search != 'undefined'){
         var busqueda=(req.query.search||"").replace(" ","%");
@@ -49,7 +74,7 @@ exports.index=function(req,res){
 
 
 exports.show = function(req, res) {
-	res.render('quizes/show', {quiz:req.quiz});
+	res.render('quizes/show', {quiz:req.quiz,errors:[]});
 };
 
 exports.answer = function(req, res) {
