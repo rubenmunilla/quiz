7dd8fc97c75f823268443abe99a109a6007abd24
Rubén <rubenmunilla@gmail.com>
2015-08-03 17:31:28 +0200

    Crear preguntas
    Formulario controlador y vistas para crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,5 +1,21 @@
 var models = require('../models/models.js');
 
+exports.new = function(req, res) {
+  var quiz = models.Quiz.build( 
+    {pregunta: "Pregunta", respuesta: "Respuesta"}
+  );
+
+  res.render('quizes/new', {quiz: quiz, errors: []});
+};
+
+exports.create = function(req, res) {
+  var quiz = models.Quiz.build( req.body.quiz);
+
+  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+	  res.redirect('/quizes');
+  })
+};
+
 exports.load = function(req, res,next,quizId) {
 	models.Quiz.findById(quizId).then(
 		function(quiz){
